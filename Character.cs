19a32ae71d6ebf8ed9ab0c0c44d5b194c62e12d1
drewh <drewherrold@gmail.com>
2018-09-21 07:48:26 -0700

    eod commit - refactoring

--- Character.cs
+++ Character.cs
@@ -4,7 +4,12 @@ namespace GeneratedDragons_Source
 {
     class Character
     {
-        public bool type {get; set;}
+        public string Name{get; private set;}
+        public string Race{get; private set;}
+        public int RaceType{get; set;}
+        public string Class{get; private set;}
+        public int ClassType{get; private set;}
+        public bool type {get; private set;}
         public int STR {get; set;}
         public int DEX {get; set;}
         public int CON {get; set;}
@@ -23,6 +28,13 @@ namespace GeneratedDragons_Source
                     HPCurrent = HPMax;
                 }
             }
+        }
+
+
+        public Character(string nameIn, string classIn, string raceIn, int typeIn)
+        {
+            Name = nameIn;
+            Class = classIn;
         }
     }
 }
